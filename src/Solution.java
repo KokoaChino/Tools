bf5227d769805c06dcb7a0a5855a06fc12534260
Kokoa_Chino <2178740980@qq.com>
2024-09-09 15:41:42 +0800

    [1.0] Initial

--- src/Solution.java
+++ src/Solution.java
@@ -4,19 +4,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 
-
-
-class ListNode {
-    int val;
-    ListNode next;
-   ListNode() {}
-     ListNode(int val) { this.val = val; }
-   ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
 class Solution {
-    public ListNode mergeNodes(ListNode head) {
-        ListNode
-    }
-}
 
+}
