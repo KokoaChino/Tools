183bc4bd37a59534544737de2a48e141c425a702
Kokoa_Chino <2178740980@qq.com>
2024-10-02 15:47:44 +0800

    [1.2] ADD ZFunction

--- src/Solution.java
+++ src/Solution.java
@@ -4,25 +4,22 @@ import java.util.*;
 
 
 class Solution {
-    public int minSpeedOnTime(int[] dist, double hour) {
-        int n = dist.length;
-        int left = 1, right = (int) 1e7;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (check(dist, hour, mid)) right = mid - 1;
-            else left = mid + 1;
-        }
-        return left;
+    public long sumScores(String s) {
+        System.out.println(Arrays.toString(calZ(s)));
+        return Arrays.stream(calZ(s)).mapToLong(Long::valueOf).sum();
     }
 
-    private boolean check(int[] dist, double hour, int x) {
-        int n = dist.length;
-        double s = 0;
-        for (int i = 0; i < n - 1; i++) {
-            s += Math.ceil((double) dist[i] / x);
-            if (s > hour) return false;
+    public int[] calZ(String s) {
+        int n = s.length();
+        int[] z = new int[n];
+        int l = 0, r = 0;
+        for (int i = 1; i < n; i++) {
+            if (i <= r) z[i] = Math.min(z[i - l], r - i + 1);
+            while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i])) {
+                l = i; r = i + z[i];
+                z[i]++;
+            }
         }
-        s += (double) dist[n - 1] / x;
-        return s <= hour;
+        return z;
     }
 }
--- src/Tools/ZFunction.java
+++ src/Tools/ZFunction.java
@@ -4,6 +4,7 @@ public class ZFunction { // Z 函数
     public int[] calZ(String s) {
         int n = s.length();
         int[] z = new int[n];
+        z[0] = n;
         int l = 0, r = 0;
         for (int i = 1; i < n; i++) {
             if (i <= r) z[i] = Math.min(z[i - l], r - i + 1);
