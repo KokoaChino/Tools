2a777a84b7e3a71f565b65761583092e49446b50
Kokoa_Chino <2178740980@qq.com>
2024-10-10 12:07:16 +0800

    [1.3] ADD LogTrick

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,3 @@
 import jdk.jshell.spi.SPIResolutionException;
 
 import java.util.*;
-
-class Solution {
-    public long countSubarrays(int[] nums, int k) {
-        int n = nums.length;
-        long ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == k) ans++;
-            for (int j = i - 1; j >= 0; j--) {
-                if ((nums[j] & nums[i]) == nums[j]) {
-                    if (nums[j] == k) ans += j;
-                    break;
-                }
-                nums[j] &= nums[i];
-            }
-        }
-        return ans;
-    }
-}
